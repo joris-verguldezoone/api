3781d77f5992ca4ab61a86df4eea1caf55706c05
Mohamed Marwane Bellagha <mohamed-marwane.bellagha@laplateforme.io>
2021-12-06 15:51:04 +0100

    commit pour hardjojo

--- src/Controller/ShunController.php
+++ src/Controller/ShunController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\HubInterface;
+use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\Update;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ShunController extends AbstractController
@@ -16,5 +19,15 @@ class ShunController extends AbstractController
         return $this->render('shun/index.html.twig', [
             'controller_name' => 'ShunController',
         ]);
+    }
+    /**
+     * @Route("/ping", name="ping", methods="POST")
+     */
+    public function ping( HubInterface $hubInterface):Response
+    {
+        $update= new Update("http://localhost/.well-known/mercure/ui/",
+            json_encode(['status' => 'OutOfStock']));
+        $hubInterface->publish($update);
+        return $this->redirectToRoute('home');
     }
 }
